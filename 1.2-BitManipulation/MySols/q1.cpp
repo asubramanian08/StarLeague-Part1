115ead866ba3d9441a2f530e86680ab5d85032cd
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-06-14 09:50:46 -0400

    shell of organization

---
+++ 1.2-BitManipulation/MySols/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    //read
+#ifndef HOME
+    freopen(".in", "r", stdin);
+    freopen(".out", "w", stdout);
+#endif
+
+    return 0;
+}
